32d9af24f559c32e44504b37f504c983b6bd6b68
Milap Prajapati <milapprajapati707070@gmail.com>
2024-04-12 13:16:55 -0400

    Create Model and screen Directory.

---
+++ lib/model/candidate.dart
@@ -0,0 +1,11 @@
+class Candidate {
+  final String name;
+  final String profilePicture;
+  final String headline;
+
+  Candidate({
+    required this.name,
+    required this.profilePicture,
+    required this.headline,
+  });
+}
