fbf7f06f2b97ab6e400320883b82ac55f2903f76
Hanee1998 <163680115+Hanee1998@users.noreply.github.com>
2024-04-19 21:40:06 -0400

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:group_project_flutter/main.dart';
+import 'package:dflutter_main/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
